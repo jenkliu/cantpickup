6d081b757c87868ccf9c2c3192b712ba0f31aa32
Jen Liu <jkliu@mit.edu>
2014-02-16 01:41:28 -0500

    phone confirmation works

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ import httplib2
 import logging
 import os
 import datetime as dt
+import simplejson as json
 
 from apiclient import discovery
 from oauth2client import appengine
@@ -55,6 +56,10 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 # on the API Access tab on the Google APIs
 # Console <http://code.google.com/apis/console>
 CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
+
+#TWILIO
+PARENT_ACCOUNT_SID = twilio_auth.get_parent_account_sid()
+PARENT_AUTH_TOKEN = twilio_auth.get_parent_auth_token()
 
 # Helpful message to display in the browser if the CLIENT_SECRETS file
 # is missing.
@@ -113,41 +118,49 @@ class PhoneInput(webapp2.RequestHandler):
   def get(self):
     template = JINJA_ENVIRONMENT.get_template('phone_input.html')
     self.response.write(template.render())
-    
+
   def post(self):
     # create a user entry
-    userstore_name = self.reqest.get('userstore_name',
+    userstore_name = self.request.get('userstore_name',
                                   DEFAULT_USERSTORE_NAME)
     account = Account(parent = userstore_key(userstore_name))
     account.user_id = get_current_uid()
-
+    print get_current_uid()
     # text the confirmation code
-    client = TwilioRestClient(parent_account_sid, parent_auth_token)
+    # client = TwilioRestClient(PARENT_ACCOUNT_SID, PARENT_AUTH_TOKEN)
     phone_no = self.request.get('phone_no')
     code=string.ascii_lowercase+string.digits
     verification_code=''.join(random.choice(code) for i in range(8))
     message="What's up? Enter %s on the sign up page to verify your Can't Pickup account! Thanks :)"%(verification_code)   
-    message = client.messages.create(to=phone_no, from_="+15402239053",
-                                         body=message)
+    # message = client.messages.create(to=phone_no, from_="+15402239053",
+                                         # body=message)
     
     # store the user_id and text conf code
     account.phone_no = phone_no
     account.text_conf = verification_code
+    account.put()
 
 class ConfirmCode(webapp2.RequestHandler):
   def post(self):
     conf_input = self.request.get('conf')
-    account = Account.get_by_id(get_current_uid())
+    print "conf: ", conf_input
+    account = Account.query(Account.user_id==get_current_uid()).get()
     if account:
-      if account.text_conf == conf_input:
-        # success
-        # create twilio account
-        client = TwilioRestClient(parent_account_sid, parent_auth_token)
-        subaccount = client.accounts.create()
-        account.twilio_sid = subaccount.sid
-        # redirect to next step: add recording handler?
-      # else:
-        # error
+      print account.text_conf, conf_input
+    if account and account.text_conf == conf_input:
+      # success
+      # create twilio account
+      client = TwilioRestClient(PARENT_ACCOUNT_SID, PARENT_AUTH_TOKEN)
+      subaccount = client.accounts.create()
+      account.twilio_sid = subaccount.sid
+      account.put()
+      # pass
+      # redirect to next step: add recording handler?
+    else:
+      response = {'error':'wrong code'}
+      JSON = json.dumps(response)
+      self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+      self.response.out.write(JSON)
 
 class AddRecordingHandler(webapp2.RequestHandler):
   @decorator.oauth_aware
